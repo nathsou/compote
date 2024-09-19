265dbe6fe44c78bada67168629e615f55b031514
Nathan Soufflet <nathsoufflet@gmail.com>
2024-09-19 23:12:02 +0200

    Compile binary operators

--- src/main/main.mjs
+++ src/main/main.mjs
@@ -1,25 +1,37 @@
-import { readFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
 
 const wasmBuffer = readFileSync('./target/wasm/release/build/main/main.wasm');
-const sourceFile = readFileSync('./examples/return_2.c', 'utf-8');
+const sourceFile = readFileSync('./examples/binaryop.c', 'utf-8');
+const outFile = './out/binaryop.s';
 
-const { log, flush } = (() => {
-  let buffer = [];
+const { log, flush, outputChar } = (() => {
+  let printBuffer = [];
+  let outputBuffer = [];
 
   function flush() {
-    if (buffer.length > 0) {
-      console.log(new TextDecoder("utf-16").decode(new Uint16Array(buffer).valueOf()));
-      buffer = [];
+    if (printBuffer.length > 0) {
+      console.log(new TextDecoder("utf-16").decode(new Uint16Array(printBuffer).valueOf()));
+      printBuffer = [];
     }
   }
 
   function log(ch) {
     if (ch == '\n'.charCodeAt(0)) { flush(); }
     else if (ch == '\r'.charCodeAt(0)) { /* noop */ }
-    else { buffer.push(ch); }
+    else { printBuffer.push(ch); }
   }
 
-  return { log, flush };
+  function outputChar(ch, done) {
+    outputBuffer.push(ch);
+
+    if (done) {
+      const str = new TextDecoder("utf-16").decode(new Uint16Array(outputBuffer).valueOf());
+      console.log(str);
+      writeFileSync(outFile, str);
+    }
+  }
+
+  return { log, flush, outputChar };
 })();
 
 const importObject = {
@@ -31,10 +43,11 @@ const importObject = {
       if (index >= sourceFile.length) {
         return 0;
       }
-
+      
       return sourceFile.charCodeAt(index);
     },
     get_source_file_length: () => sourceFile.length,
+    output_char: outputChar,
   },
 };
 
